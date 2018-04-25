82d343072aeb094ed7bdc72259ea39d85fa633bb
fri13th <tenderain@gmail.com>
2018-04-25 14:06:09 +0900

    fix default setting about pagination

--- src/Ling/Orm/Paginate.php
+++ src/Ling/Orm/Paginate.php
@@ -4,15 +4,15 @@ namespace Ling\Orm;
 
 class Paginate { // provide improved pagination
     // input for pagination
-    const DEFAULT_CURRENT_PAGE = 10; // can be overrided
+    const DEFAULT_CURRENT_PAGE = 1; // can be overrided
+    const DEFAULT_PAGINATION_SIZE = 10;
     const DEFAULT_LIST_SIZE = 10;
-    const DEFAULT_PAGINATION_SIZE = 5;
 
     public $currentPage;
-    public $listSize;
     public $paginationSize;
-    public $keyword;
-    public $criteria;
+    public $listSize; // this is for server-side pagination template, not important in these days
+    public $keyword; // this must be set after initialize
+    public $criteria; // this must be set after initialize
 
     // output
     public $totalCount;
@@ -31,15 +31,16 @@ class Paginate { // provide improved pagination
     protected $useLastSkip;
     protected $totalPage;
 
-    public function __construct($currentPage, $keyword = NULL, $criteria = NULL, int $listSize = 0, int $paginationSize = 0){
+    public function __construct($currentPage, int $paginationSize = 0, int $listSize = 0){
         $this->currentPage = (!$currentPage) ? self::DEFAULT_CURRENT_PAGE : $currentPage ;
-        $this->listSize = $listSize ?: self::DEFAULT_LIST_SIZE;
         $this->paginationSize = $paginationSize ?: self::DEFAULT_PAGINATION_SIZE;
-        $this->startAt = ($this->currentPage - 1)*$this->paginationSize;
-        $this->endAt = $this->currentPage * $this->paginationSize;
+        $this->listSize = $listSize ?: self::DEFAULT_LIST_SIZE;
         $this->validIndex = true;
         $this->totalCount = 0;
         $this->list = array();
+
+        $this->startAt = ($this->currentPage - 1)*$this->paginationSize;
+        $this->endAt = $this->currentPage * $this->paginationSize;
     }
 
     public function setList($list){$this->list = $list;}
